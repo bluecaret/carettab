4b61dab4bcd3fcb4567168cc06681dd0c9b99c5f
John Hancock <johnhancock@outlook.com>
2015-06-09 23:46:09 -0700

    Add analytics and screenshots

--- carettab/js/main.js
+++ carettab/js/main.js
@@ -234,3 +234,21 @@ $(document).ready(function(){
 	$(".green").css({"background-color": themeGreen});
 	$(".blue").css({"background-color": themeBlue});
 });
+
+
+// GOOGLE ANALYTICS
+
+var _AnalyticsCode = 'UA-63895389-1';
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();

dc10e8afef1ff73b1995b1a38adf857b0eac6f6a
bluecaret <john0404@gmail.com>
2017-11-29 20:55:22 -0800

    get old links and add to new link format

--- src/js/links.js
+++ src/js/links.js
@@ -4,6 +4,54 @@ var addBtn = document.getElementById('s-link-add');
 var newLabel = document.getElementById('s-link-label');
 var newUrl = document.getElementById('s-link-url');
 
+// Temp code to pull in old links and add to new links
+storage.get([
+  'links',
+  's1stLink','s1stLinkLabel','s1stLinkUrl',
+  's2ndLink','s2ndLinkLabel','s2ndLinkUrl',
+  's3rdLink','s3rdLinkLabel','s3rdLinkUrl',
+  's4thLink','s4thLinkLabel','s4thLinkUrl',
+  's5thLink','s5thLinkLabel','s5thLinkUrl',
+  's6thLink','s6thLinkLabel','s6thLinkUrl'
+  ], function(data) {
+  var allLinks = data.links ? data.links : [];
+  if (data.s1stLink == 'on' && data.s1stLinkLabel && data.s1stLinkUrl) {
+    var link1 = new link(data.s1stLinkLabel, data.s1stLinkUrl);
+    allLinks.push(link1);
+  }
+  if (data.s2ndLink == 'on' && data.s2ndLinkLabel && data.s2ndLinkUrl) {
+    var link2 = new link(data.s2ndLinkLabel, data.s2ndLinkUrl);
+    allLinks.push(link2);
+  }
+  if (data.s3rdLink == 'on' && data.s3rdLinkLabel && data.s3rdLinkUrl) {
+    var link3 = new link(data.s3rdLinkLabel, data.s3rdLinkUrl);
+    allLinks.push(link3);
+  }
+  if (data.s4thLink == 'on' && data.s4thLinkLabel && data.s4thLinkUrl) {
+    var link4 = new link(data.s4thLinkLabel, data.s4thLinkUrl);
+    allLinks.push(link4);
+  }
+  if (data.s5thLink == 'on' && data.s5thLinkLabel && data.s5thLinkUrl) {
+    var link5 = new link(data.s5thLinkLabel, data.s5thLinkUrl);
+    allLinks.push(link5);
+  }
+  if (data.s6thLink == 'on' && data.s6thLinkLabel && data.s6thLinkUrl) {
+    var link6 = new link(data.s6thLinkLabel, data.s6thLinkUrl);
+    allLinks.push(link6);
+  }
+  storage.set({'links': allLinks }, function() { if (err) { console.log('Error Saving Link: ' + err); } });
+  storage.remove([
+    's1stLink','s1stLinkLabel','s1stLinkUrl',
+    's2ndLink','s2ndLinkLabel','s2ndLinkUrl',
+    's3rdLink','s3rdLinkLabel','s3rdLinkUrl',
+    's4thLink','s4thLinkLabel','s4thLinkUrl',
+    's5thLink','s5thLinkLabel','s5thLinkUrl',
+    's6thLink','s6thLinkLabel','s6thLinkUrl'
+  ], function() { if (err) { console.log('Error Removing Old Links: ' + err); } });
+  showLinksInterface();
+  showLinksSettings();
+});
+
 function link (theLabel, theUrl) {
   this.label = theLabel;
   this.url = theUrl;
@@ -12,10 +60,14 @@ function link (theLabel, theUrl) {
 function add() {
   storage.get('links', function(data) {
     var allLinks = data.links ? data.links : [];
-    var newLink = new link(newLabel.value, newUrl.value);
+    var checkedNewUrl = checkLink(newUrl.value);
+    var newLink = new link(newLabel.value, checkedNewUrl);
 
     allLinks.push(newLink);
     storage.set({'links': allLinks }, function() { if (err) { console.log('Error Saving Link: ' + err); } });
+
+    newLabel.value = '';
+    newUrl.value = '';
 
     showLinksInterface();
     showLinksSettings();
@@ -67,7 +119,6 @@ function showLinksInterface() {
     var allLinks = data.links;
 
     if (allLinks != undefined) {
-      console.log('start link interface: ', allLinks);
       var html = ''
 
       for(var i = 0; i < allLinks.length; i++) {

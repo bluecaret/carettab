f597dab0e3e7913dd261467c2806ad1a82abf489
bluecaret <john0404@gmail.com>
2018-05-08 21:44:39 -0700

    Angular 6.0 update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Core Imports
@@ -50,7 +50,7 @@ import { TabBookmarksComponent } from './tab/bookmarks/bookmarks.component';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     Ng2ChromeStorageModule
   ],
   providers: [

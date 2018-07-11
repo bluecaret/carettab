6b4911aee9f6147165e9acf47269e3ec14b0ae8b
bluecaret <john0404@gmail.com>
2018-07-10 21:06:31 -0700

    Time setup

---
+++ src/app/_shared/models/clock.ts
@@ -0,0 +1,53 @@
+export class Clock {
+  constructor(
+    public label: string = '',
+    public timezone: string = 'Automatic',
+    public scaling: number = 50,
+    public font: string = 'Roboto',
+    public seconds: Seconds = new Seconds(true, false, false),
+    public twentyFour: boolean = false,
+    public meridiem: Meridiem = new Meridiem(true, true),
+    public delimiter: Delimiter = new Delimiter(true, true, true),
+    public brackets: Brackets = new Brackets(true, true, '', ''),
+    public analog: Analog = new Analog(false, 10),
+  ) {}
+}
+
+export class Seconds {
+  constructor(
+    public enabled: boolean = true,
+    public dim: boolean = false,
+    public blink: boolean = false
+  ) {}
+}
+
+export class Meridiem {
+  constructor(
+    public enabled: boolean = true,
+    public dim: boolean = true
+  ) {}
+}
+
+export class Delimiter {
+  constructor(
+    public enabled: boolean = true,
+    public dim: boolean = true,
+    public blink: boolean = true
+  ) {}
+}
+
+export class Brackets {
+  constructor(
+    public enabled: boolean = true,
+    public dim: boolean = true,
+    public left: string = '',
+    public right: string = '',
+  ) {}
+}
+
+export class Analog {
+  constructor(
+    public enabled: boolean = false,
+    public style: number = 10
+  ) {}
+}
--- src/app/tab/time/time.component.ts
+++ src/app/tab/time/time.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, NgZone, Renderer2, ElementRef, ViewChild } from '@angular/core';
 import { Storage } from '../../_storage/storage.service';
+import { Clock, Delimiter } from '../../_shared/models/clock';
+import { SharedService } from '../../_shared/shared.service';
 import * as moment from 'moment';
 
 @Component({
@@ -7,25 +9,107 @@ import * as moment from 'moment';
   templateUrl: 'time.component.html'
 })
 export class TabTimeComponent implements OnInit {
-  currentTime: string;
+  currentTime: Date;
+  baseScale = 50;
 
   @ViewChild('time')
   public displayTime: ElementRef;
 
   constructor(
+    public shared: SharedService, 
     public settings: Storage, 
     private zone: NgZone, 
     private renderer: Renderer2
   ) {
-    this.zone.runOutsideAngular(() => {
+    if (this.settings.config.time.clocks.length > 0) {
+      this.shared.zoneGuess = moment.tz.guess();
       setInterval(() => {
-        this.renderer.setProperty(
-          this.displayTime.nativeElement, 
-          'textContent', 
-          this.currentTime = moment(new Date()).format('HH:mm:ss')
-        );
-      }, 1);
-    });
+        this.currentTime = new Date();
+      }, 500);
+    }
+  }
+
+  calcSize(size: number) {
+    return ((this.baseScale / 50) * size) + 'px';
+  }
+
+  getTime(c: Clock): string {
+    let hour = `
+      <div class="timeDigit hour">
+        <span>${this.getHour(c.timezone, c.twentyFour, 1)}</span>
+        <span>${this.getHour(c.timezone, c.twentyFour, 2)}</span>
+      </div>
+    `;
+    let minute = `
+      <div class="timeDigit minute">
+        <span>${this.getMinute(c.timezone, 1)}</span>
+        <span>${this.getMinute(c.timezone, 2)}</span>
+      </div>
+    `;
+    let second = c.seconds.enabled ? `
+      <div class="timeDigit second">
+        <span>${this.getSecond(c.timezone, 1)}</span>
+        <span>${this.getSecond(c.timezone, 2)}</span>
+      </div>
+    ` : '';
+    let delimiter1 = `
+      <div class="delimiter">
+        ${this.getDelimiter(c.delimiter)}
+      </div>
+    `;
+    let delimiter2 = c.seconds.enabled ? `
+      <div class="delimiter">
+        ${this.getDelimiter(c.delimiter)}
+      </div>
+    ` : '';
+
+    return hour + delimiter1 + minute + delimiter2 + second;
+  }
+
+  getHour(zone: string, twentyFour: boolean, digit: 1|2): string {
+    zone = this.getZone(zone);
+    let format: string;
+    twentyFour ? format = 'HH' : format = 'hh';
+    let time = moment(this.currentTime).tz(zone).format(format);
+    return this.splitDigits(time, digit);
+  }
+  getMinute(zone: string, digit: 1|2): string {
+    zone = this.getZone(zone);
+    let time = moment(this.currentTime).tz(zone).format('mm');
+    return this.splitDigits(time, digit);
+  }
+  getSecond(zone: string, digit: 1|2): string {
+    zone = this.getZone(zone);
+    let time = moment(this.currentTime).tz(zone).format('ss');
+    return this.splitDigits(time, digit);
+  }
+
+  splitDigits(time: string, digit: 1|2) {
+    let timeSplit = time.split('');
+    if (digit === 1) {
+      return timeSplit[0];
+    } else if (digit === 2) {
+      return timeSplit[1];
+    }
+  }
+
+  getZone(zone: string) {
+    if (zone === 'Automatic') {
+      zone = this.shared.zoneGuess;
+    }
+    return zone;
+  }
+
+  getDelimiter(delimiter: Delimiter): string {
+    if (delimiter.enabled) {
+      return ':';
+    } else {
+      return ' ';
+    }
+  }
+
+  getFont(font: string) {
+    return '"' + font + '"';
   }
 
   ngOnInit() {

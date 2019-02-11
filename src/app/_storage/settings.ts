c6a494bab5fbd2570adccfdb4314d63abaa399d5
bluecaret <john0404@gmail.com>
2019-02-10 17:46:25 -0800

    Change default quick link for Gmail so favicon will display

--- src/app/_storage/settings.ts
+++ src/app/_storage/settings.ts
@@ -42,7 +42,7 @@ export class Settings {
         },
         {
           label: 'Gmail',
-          url: 'https://www.gmail.com/'
+          url: 'https://gmail.com/'
         },
         {
           label: 'YouTube',

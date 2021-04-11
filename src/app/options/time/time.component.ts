38f5c954fa44633f10ad631dc2b03263708a34f0
John Hancock <john0404@gmail.com>
2021-04-10 19:38:05 -0700

    #197 Add GMT options

--- src/app/options/time/time.component.ts
+++ src/app/options/time/time.component.ts
@@ -19,6 +19,36 @@ export class OptionsTimeComponent {
   analog = analogStyles;
   span = span;
 
+  gmt = [
+    {label: 'GMT-0', value: 'Etc/GMT-0'},
+    {label: 'GMT-1', value: 'Etc/GMT-1'},
+    {label: 'GMT-2', value: 'Etc/GMT-2'},
+    {label: 'GMT-3', value: 'Etc/GMT-3'},
+    {label: 'GMT-4', value: 'Etc/GMT-4'},
+    {label: 'GMT-5', value: 'Etc/GMT-5'},
+    {label: 'GMT-6', value: 'Etc/GMT-6'},
+    {label: 'GMT-7', value: 'Etc/GMT-7'},
+    {label: 'GMT-8', value: 'Etc/GMT-8'},
+    {label: 'GMT-9', value: 'Etc/GMT-9'},
+    {label: 'GMT-10', value: 'Etc/GMT-10'},
+    {label: 'GMT-11', value: 'Etc/GMT-11'},
+    {label: 'GMT-12', value: 'Etc/GMT-12'},
+    {label: 'GMT-13', value: 'Etc/GMT-13'},
+    {label: 'GMT-14', value: 'Etc/GMT-14'},
+    {label: 'GMT+1', value: 'Etc/GMT+1'},
+    {label: 'GMT+2', value: 'Etc/GMT+2'},
+    {label: 'GMT+3', value: 'Etc/GMT+3'},
+    {label: 'GMT+4', value: 'Etc/GMT+4'},
+    {label: 'GMT+5', value: 'Etc/GMT+5'},
+    {label: 'GMT+6', value: 'Etc/GMT+6'},
+    {label: 'GMT+7', value: 'Etc/GMT+7'},
+    {label: 'GMT+8', value: 'Etc/GMT+8'},
+    {label: 'GMT+9', value: 'Etc/GMT+9'},
+    {label: 'GMT+10', value: 'Etc/GMT+10'},
+    {label: 'GMT+11', value: 'Etc/GMT+11'},
+    {label: 'GMT+12', value: 'Etc/GMT+12'},
+  ];
+
   constructor(
     public shared: SharedService,
     public settings: Storage,

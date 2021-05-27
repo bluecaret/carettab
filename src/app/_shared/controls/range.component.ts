6a7ee16b34c06e2d9d06481cc2ec5a6ade087989
John Hancock <john0404@gmail.com>
2021-05-26 22:31:22 -0700

    Switch settings fields to be inline vs stacked

--- src/app/_shared/controls/range.component.ts
+++ src/app/_shared/controls/range.component.ts
@@ -12,6 +12,7 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
   ],
   template: `
     <div class="range">
+      <output [value]="range"></output>
       <input
         type="range"
         [id]="name+'Id'"
@@ -21,7 +22,6 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
         [(ngModel)]="range"
         [disabled]="disabled"
         [required]="required">
-      <output [value]="range"></output>
     </div>
   `,
   host: {'class': 'customControl'}

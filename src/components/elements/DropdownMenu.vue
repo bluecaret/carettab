2daa0c1a9e75e61c8fb96ef0dac843baabbbae71
bluecaret <john0404@gmail.com>
2023-05-04 19:57:41 -0700

    fix path

--- src/components/elements/DropdownMenu.vue
+++ src/components/elements/DropdownMenu.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, watch, nextTick } from 'vue'
-import { useDetectOutsideClick } from '../helpers/outsideClick'
+import { useDetectOutsideClick } from '@/helpers/outsideClick'
 
 const ddBtn = ref()
 const ddMenu = ref()

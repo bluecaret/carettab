4367f67a7e6249b27bd44f9e1a2016f31e668b53
bluecaret <john0404@gmail.com>
2023-06-01 14:38:04 -0700

    update eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
   ignorePatterns: ['src/assets/ExtPay.js'],
   rules: {
     'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off',
     semi: ['warn', 'never'],
   },
 }

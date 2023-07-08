86ffe33713dc94fb25d0b5ecdc9bb88ffeb55c80
bluecaret <john0404@gmail.com>
2023-07-07 20:50:08 -0700

    change chunking strategy due to incompatibility with watch

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import { resolve, dirname } from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import { crx } from '@crxjs/vite-plugin'
@@ -18,38 +18,10 @@ export default defineConfig({
     sourcemap: false,
     emptyOutDir: true,
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          fa: [
-            '@fortawesome/fontawesome-svg-core',
-            '@fortawesome/free-solid-svg-icons',
-            '@fortawesome/vue-fontawesome',
-          ],
-          vue: ['vue'],
-          vuetools: ['pinia', 'vue-i18n', 'vuedraggable'],
-          vendors: ['luxon', 'lodash', 'convert', 'openai'],
-          de: ['./src/locales/de.json'],
-          en: ['./src/locales/en.json'],
-          es: ['./src/locales/es.json'],
-          fr: ['./src/locales/fr.json'],
-          he: ['./src/locales/he.json'],
-          it: ['./src/locales/it.json'],
-          ja: ['./src/locales/ja.json'],
-          ko: ['./src/locales/ko.json'],
-          pt: ['./src/locales/pt.json'],
-          ru: ['./src/locales/ru.json'],
-          sv: ['./src/locales/sv.json'],
-          uk: ['./src/locales/uk.json'],
-          ur: ['./src/locales/ur.json'],
-          vi: ['./src/locales/vi.json'],
-          zh: ['./src/locales/zh.json'],
-        },
-      },
-    },
   },
   plugins: [
     vue(),
+    splitVendorChunkPlugin(),
     crx({ manifest }),
     VueI18nPlugin({
       include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),

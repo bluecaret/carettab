ce1de8af3b96a611e626e7a439af05ee03ba988b
bluecaret <dev@bluecaret.com>
2023-08-06 11:26:06 -0700

    Bump version to 4.1.0

--- manifest.config.js
+++ manifest.config.js
@@ -6,8 +6,8 @@ const getManifestSettings = (env) => {
     short_name: 'CaretTab',
     name: env.mode === 'beta' ? 'CaretTab BETA' : '__MSG_appName__',
     description: env.mode === 'beta' ? 'Beta version of the New Tab extension, CaretTab' : '__MSG_appDesc__',
-    version: `4.0.4`,
-    version_name: `4.0.4`,
+    version: `4.1.0`,
+    version_name: `4.1.0`,
     default_locale: 'en',
     icons: {
       16: 'img/icon16.png',
